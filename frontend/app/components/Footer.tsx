70099d29bcbebdb34e11eb81e67d310936de832a
MozartOnWeb <mozartontheweb@gmail.com>
2025-07-17 17:11:37 +0000

    footer and header are done

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import styles from "./Footer.module.scss";
+
 export default function Footer() {
-  return <footer className=""></footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <Link href="/">LinkedIn</Link>
+        <Link href="/">Github</Link>
+        <Link href="/">Twitter</Link>
+      </div>
+
+      <p>2025 Mandjou Dama. All rights reserved.</p>
+    </footer>
+  );
 }
--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,3 +1,5 @@
+import styles from "./Header.module.scss";
+
 import Link from "next/link";
 import { settingsQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/live";
@@ -7,5 +9,42 @@ export default async function Header() {
     query: settingsQuery,
   });
 
-  return <header className=""></header>;
+  return (
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
+        M ― D
+      </Link>
+
+      <div className={styles.theme_switchers}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className={`${styles.button} ${styles.active}`}
+        >
+          <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          className={`${styles.button}`}
+        >
+          <circle cx={12} cy={12} r={4} />
+          <path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41" />
+        </svg>
+      </div>
+    </header>
+  );
 }
